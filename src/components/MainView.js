10b3987443d1eb67f5962fc346ed7228a2b2eb90
Logan Levin <loganjlevin@gmail.com>
2023-03-31 12:39:59 -0700

    flex styling for sidebar mainview

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -6,18 +6,23 @@ import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
 import CodeSamples from './CodeSamples';
+import './MainView.scss';
 
 const MainView = () => {
   return (
     <BrowserRouter>
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/code" element={<CodeSamples />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="layout-container">
+        <Sidebar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/code" element={<CodeSamples />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
       <Footer />
     </BrowserRouter>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,17 @@
-import { Nav, Navbar, Container } from 'react-bootstrap';
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
+import './Sidebar.scss';
 
 const Sidebar = () => {
   const location = useLocation();
-
+  //col-md-12 d-md-block
   return (
-    <Navbar>
-      <Container>
+    <Navbar bg="light" expand="lg">
+      <Container className="sidebar">
+        <Navbar.Brand href="#home">&nbsp;</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav
-            activeKey={location.pathname}
-            className="col-md-12 d-md-block sidebar bg-primary"
-          >
+          <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/projects">Projects</Nav.Link>
             <Nav.Link href="/code">Code Samples</Nav.Link>
@@ -22,6 +21,21 @@ const Sidebar = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    // <Navbar>
+    //   <Container>
+    //     <Navbar.Brand>TempText</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //       <Nav activeKey={location.pathname} className=" sidebar bg-primary">
+    //         <Nav.Link href="/">Home</Nav.Link>
+    //         <Nav.Link href="/projects">Projects</Nav.Link>
+    //         <Nav.Link href="/code">Code Samples</Nav.Link>
+    //         <Nav.Link href="/about">About Me</Nav.Link>
+    //         <Nav.Link href="/contact">Contact</Nav.Link>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
   );
 };
 
