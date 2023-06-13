e4c7a565309e4f27abad90595300cee7e07f41a0
Logan Levin <loganjlevin@gmail.com>
2023-06-13 14:20:51 -0700

    updated the about me page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,31 @@
+import "./About.scss";
+import selfie from "../img/selfie.jpg";
+
 const About = () => {
-  return <h1>About Me</h1>;
+  return (
+    <div className="about">
+      <h1>About Me</h1>
+      <div className="about-content">
+        <img src={selfie} alt="logan-profile" />
+        <div>
+          <p>
+            Since an early age I have been a tech enthusiast. Tinkering with
+            electornics, exploring software features and building my own gaming
+            PC were all hobbies during high school. But not unil I took my first
+            Computer Science class in college did I fall in love with solving
+            problems with computers. Though my career has taken a different
+            course, coding has always been my primary hobby and passion and that
+            is why I'm making the career change to software desvelopment.
+          </p>
+          <p>
+            When I'm not finding novel solutions to computer problems I can be
+            found cooking delicious food, rollerblading, playing Dungeons &
+            Dragons, PC games or chess.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
