fb8e4ec17a32d4c771c715ddc258da314c2ee1de
Logan Levin <loganjlevin@gmail.com>
2023-06-25 13:22:24 -0700

    updated projects to include tech icons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import { Row, Col, Button, Card, Container } from 'react-bootstrap';
+import { Row, Col, Button, Card, Container, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import myflix from '../img/myflix.png';
 import myflixAngular from '../img/myflixAngular.png';
@@ -7,6 +7,24 @@ import meet from '../img/meetapp.png';
 import './Projects.scss';
 import movieApi from '../img/movieApi.png';
 import chatApp from '../img/chatApp.png';
+import react from '../img/react.png';
+import express from '../img/express.png';
+import mongodb from '../img/mongo.png';
+import bootstrap from '../img/bootstrap.png';
+import reactRouter from '../img/react-router.png';
+import angular from '../img/angular.png';
+import material from '../img/material.png';
+import typescript from '../img/typescript.png';
+import google from '../img/google.png';
+import aws from '../img/aws.webp';
+import jest from '../img/jest.png';
+import expo from '../img/expo.png';
+import firebase from '../img/firebase.png';
+import node from '../img/node.png';
+import jwt from '../img/jwt.png';
+import html from '../img/html.png';
+import css from '../img/css.png';
+import javascript from '../img/javascript.png';
 
 const Projects = () => {
   return (
@@ -17,6 +35,11 @@ const Projects = () => {
             <Card.Img variant="top" src={myflix} />
             <Card.Body>
               <Card.Title>MyFlix - React</Card.Title>
+
+              <Image className="icon" src={react} title="React" />
+              <Image className="icon" src={bootstrap} title="Bootstrap" />
+              <Image className="icon" src={reactRouter} title="React Router" />
+
               <Card.Text>
                 MERN stack web application where users can create a profile,
                 search a list of movies, view a movie's details, and add movies
@@ -48,6 +71,11 @@ const Projects = () => {
             <Card.Img variant="top" src={myflixAngular} />
             <Card.Body>
               <Card.Title>MyFlix - Angular</Card.Title>
+
+              <Image className="icon" src={angular} title="Angular" />
+              <Image className="icon" src={typescript} title="Typescript" />
+              <Image className="icon" src={material} title="Angular Material" />
+
               <Card.Text>
                 MEAN stack web application where users can create a profile,
                 search a list of movies, view a movie's details, and add movies
@@ -79,6 +107,12 @@ const Projects = () => {
             <Card.Img variant="top" src={meet} />
             <Card.Body>
               <Card.Title>Meet App</Card.Title>
+
+              <Image className="icon" src={react} title="React" />
+              <Image className="icon" src={google} title="Google API" />
+              <Image className="icon" src={aws} title="AWS Lambda" />
+              <Image className="icon" src={jest} title="Jest" />
+
               <Card.Text>
                 Progressive web application that displays data from the Google
                 Calendar API. Users see charts based on event location and
@@ -110,6 +144,11 @@ const Projects = () => {
             <Card.Img variant="top" src={chatApp} />
             <Card.Body>
               <Card.Title>Chat App</Card.Title>
+
+              <Image className="icon" src={react} title="React" />
+              <Image className="icon" src={expo} title="Expo" />
+              <Image className="icon" src={firebase} title="Firebase" />
+
               <Card.Text>
                 A React Native mobile application that allows users to chat,
                 share pictures, and send their locations.
@@ -131,6 +170,12 @@ const Projects = () => {
             <Card.Img variant="top" src={movieApi} />
             <Card.Body>
               <Card.Title>Movie API</Card.Title>
+
+              <Image className="icon" src={node} title="Node JS" />
+              <Image className="icon" src={express} title="Express JS" />
+              <Image className="icon" src={mongodb} title="MongoDB" />
+              <Image className="icon" src={jwt} title="Jason Web Token" />
+
               <Card.Text>
                 A RESTful API backend for the MyFlix client using Node.js,
                 Express, and MongoDB.
@@ -152,6 +197,10 @@ const Projects = () => {
             <Card.Img variant="top" src={pokedex} />
             <Card.Body>
               <Card.Title>Pokedex</Card.Title>
+
+              <Image className="icon" src={html} title="HTML 5" />
+              <Image className="icon" src={css} title="CSS 3" />
+              <Image className="icon" src={javascript} title="Javascript" />
               <Card.Text>
                 Web application that displays data from the{' '}
                 <Link
